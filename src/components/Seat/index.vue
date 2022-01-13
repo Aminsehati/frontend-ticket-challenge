8039002bbd1e0273ab9e0e1704f23d89e4888bea
Amin Sehati <Aminsehati70@gmail.com>
2022-01-14 02:05:54 +0330

    create components spinner and complete web app sold ticket

---
+++ src/components/Seat/index.vue
@@ -0,0 +1,37 @@
+<template>
+  <div
+    v-for="(seat, indexCloumn) in seatItem"
+    :key="indexCloumn"
+    class="mb-20 cursor-pointer"
+    @click="ReservationItem(indexCloumn)"
+  >
+    <img
+      :src="icons.seatIcon"
+      width="40"
+      height="40"
+      :class="{ active: seat === 1 }"
+    />
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    seatItem: {
+      type: Array,
+      default: () => [],
+    }
+  },
+  data() {
+    return {
+      icons: {
+        seatIcon: require("../../assets/icons/seat.svg"),
+      },
+    };
+  },
+  methods: {
+    ReservationItem(indexCloumn) {
+      this.$emit("ReservationItem",indexCloumn);
+    },
+  },
+};
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,113 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="wrapper">
+      <Spinner v-if="filters.loading" />
+      <div class="content" v-else>
+        <div class="item" v-for="(item, indexRow) in items" :key="indexRow">
+          <div
+            v-for="(seatItem, indexCloumn) in item"
+            :key="indexCloumn"
+            class="mb-20 cursor-pointer"
+            @click="ReservationItem(indexCloumn, indexRow)"
+          >
+            <img
+              :src="icons.seatIcon"
+              width="40"
+              height="40"
+              :class="{ active: seatItem === 1 }"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import axios from "axios";
+import Seat from "../components/Seat";
+import Spinner from "../components/Spinner";
+import "./style.css";
 export default {
-  name: 'Home',
   components: {
-    HelloWorld
-  }
-}
+    Seat,
+    Spinner,
+  },
+  name: "Home",
+  data() {
+    return {
+      id: null,
+      items: [],
+      filters: {
+        loading: false,
+      },
+      icons: {
+        seatIcon: require("../assets/icons/seat.svg"),
+      },
+    };
+  },
+  mounted() {
+    this.init();
+  },
+  methods: {
+    async getRandomIdMap() {
+      try {
+        const httpResponse = await axios.get(
+          "https://ticket-challange.herokuapp.com/map"
+        );
+        const data = httpResponse?.data?.data?.map_ids;
+        const rndInt = Math.floor(Math.random() * 6) + 1;
+        this.id = data[rndInt];
+        console.log(this.id);
+      } catch (error) {
+        console.log(error);
+      }
+    },
+    async getSeatItems() {
+      this.filters.loading = true;
+      try {
+        if (this.id) {
+          const config = {
+            headers: {
+              "Access-Control-Allow-Origin": "*",
+              "Access-Control-Allow-Methods":
+                "GET,PUT,POST,DELETE,PATCH,OPTIONS",
+            },
+          };
+          const httpResponse = await axios.get(
+            `https://ticket-challange.herokuapp.com/map/${this.id}`,
+            config
+          );
+          const data = httpResponse?.data?.data;
+          this.items = data.slice(0, 10);
+          this.filters.loading = false;
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    },
+    async init() {
+      await this.getRandomIdMap();
+      await this.getSeatItems();
+    },
+    ReservationItem(indexRow, indexCloumn) {
+      this.buyTikcet(indexRow, indexCloumn);
+    },
+    async buyTikcet(y, x) {
+      try {
+        var formData = new FormData();
+        formData.append("x", x);
+        formData.append("y", y);
+        const httpResponse = await axios.post(
+          `
+        https://ticket-challange.herokuapp.com/map/${this.id}/ticket`,
+          formData
+        );
+        this.items[x][y] = 1;
+      } catch (error) {
+        console.log(error);
+      }
+    },
+  },
+};
 </script>
